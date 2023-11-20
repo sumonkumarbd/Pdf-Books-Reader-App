87274aa9c23d9dcd0957e034df081a5041a4f764
sumonkumarbd <sumonkmr.dev@gmail.com>
2023-11-20 18:20:34 +0600

    Final. Now Just setPDF Links

---
+++ app/src/main/java/com/sumonkmr/coustompdfreaderapps/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.sumonkmr.coustompdfreaderapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+    ImageView introLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        introLogo = findViewById(R.id.introLogo);
+
+        Animation slideIN = AnimationUtils.loadAnimation(this,R.anim.left_to_right);
+                        slideIN.setDuration(2000);
+        introLogo.startAnimation(slideIN);
+
+
+        Handler handler = new Handler();
+        Runnable runnable = () -> {
+            Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        };
+
+        handler.postDelayed(runnable,3000);
+    }
+}
